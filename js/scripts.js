f83987b2075ffbb4acee26c8a8d9fd033d2f2346
rizeup88 <marinersrock88@yahoo.com>
2019-07-23 11:46:03 -0700

    This new commit will now add a feature where the image of the animal chosen follows after the meow or bark returned from your choice, which can be removed by clicking on the image.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
   $("button#meow").click(function() {
     $("ul").prepend("<li>Meow!</li>");
+    $("ul").append('<img src="images/grumpycat1.jpg" alt="Grumpy Cat">');
+    $("ul").children("img").first().click(function() {
+  		$(this).remove();
+	});
   });
 
   $("button#bark").click(function() {
     $("ul").prepend("<li>Woof!</li>");
+    $("ul").append('<img src="images/golden1.jpg" alt="Golden Retriever pup">');
+    $("ul").children("img").first().click(function() {
+  		$(this).remove();
+  	});
   });
 });
